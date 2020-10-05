4279bb52cdff8b854f6fd9d7c9e61271864c0377
mvashishth <vashishthmayur@gmail.com>
2020-10-05 21:28:39 +0530

    reverted to last working blog

---
+++ build/precache-manifest.a04c3452452a001ebabce6d53b4a8dd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101d32e588969404eda6f77cb2a83e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55c03ec26c7323834380",
+    "url": "/static/css/main.6aa7d348.chunk.css"
+  },
+  {
+    "revision": "f414f0d38893d4e3dac9",
+    "url": "/static/js/2.8aa33d30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8aa33d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c03ec26c7323834380",
+    "url": "/static/js/main.83c501e4.chunk.js"
+  },
+  {
+    "revision": "7162bd0edde6ccb35952",
+    "url": "/static/js/runtime-main.da0b37fe.js"
+  },
+  {
+    "revision": "d7e0fb4b54cdd09eb92aafcd851f2ee8",
+    "url": "/static/media/main.d7e0fb4b.jpg"
+  },
+  {
+    "revision": "7ad7009215d1185b6431e000a7df20a9",
+    "url": "/static/media/resume.7ad70092.pdf"
+  }
+]);
